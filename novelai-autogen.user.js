fc51e499455963c7c5cbd5078baafcd4b61216ac
= <=>
2024-05-24 16:23:23 -0600

    update version number

--- novelai-autogen.user.js
+++ novelai-autogen.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NAI Autogenerate
 // @namespace    http://tampermonkey.net/
-// @version      2024-05-01.03
+// @version      2024-05-24.01
 // @description  Adds a button to autoclick the generate button if available
 // @author       mothmind
 // @match        https://novelai.net/*
